e4d64fba98fdec9542ca6d3e74e2530a4b057149
KGossage11 <kylegossage@gmail.com>
2025-10-28 14:19:32 -0700

    Added basic flask app for render

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return jsonify({"message": "Render Flask backend is running!"})
+    return jsonify({"message": "Flask backend is running!"})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
